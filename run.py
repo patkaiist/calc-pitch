68c2df4cc6224abd0ab231fe193c185591b4cf00
KP van Dam 柯禕藍 <patkaiist@gmail.com>
2024-09-04 11:02:06 +0200

    data files into different folders

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
+import numpy as np
+from scipy.interpolate import make_interp_spline
+
 import os
+folder = 'wai'
 from os.path import isfile, join
-filelist = [f for f in os.listdir("./data") if isfile(join("./data", f))]
+filelist = [f for f in os.listdir("./" + folder) if isfile(join("./" + folder, f))]
 for filename in filelist:
-    with open("./data/" + filename) as f:
+    with open("./" + folder + "/" + filename) as f:
         if "--undefined--" in f.read():
             print(filename)
 import pandas
@@ -10,21 +14,28 @@ import math
 import matplotlib.pyplot as plt
 all_data = pandas.DataFrame()
 for filename in filelist:
-    data = pandas.read_csv("./data/" + filename, sep="   ", engine="python")
+    data = pandas.read_csv(folder + "/" + filename, sep="   ", engine="python")
     data["Semitones"] = 12 * data["F0_Hz"].apply(
         lambda x: math.log(x / data["F0_Hz"].mean(), 2)
     )
     all_data = pandas.concat([all_data, data], ignore_index=True)
 plt.figure(figsize=(10, 6))
 for filename in filelist:
-    data = pandas.read_csv("./data/" + filename, sep="   ", engine="python")
+    data = pandas.read_csv("./" + folder + "/" + filename, sep="   ", engine="python")
     data["Semitones"] = 12 * data["F0_Hz"].apply(
         lambda x: math.log(x / data["F0_Hz"].mean(), 2)
     )
     data["Cents"] = round(data["Semitones"] * 100).astype(int)
     data["ZScore"] = (data["Semitones"] - all_data["Semitones"].mean()) / all_data["Semitones"].std()
     data["Time"] = data["Time_s"] - data["Time_s"].min()
-    plt.plot(data["Time"], data["ZScore"], label=filename)
+
+    x = np.linspace(data["Time"].min(), data["Time"].max(), 300)
+    spl = make_interp_spline(data["Time"], data["ZScore"], k=2)
+    y = spl(x)
+
+    plt.plot(x,y, label=filename)
+
+
 plt.title("log z-Score contours")
 plt.xlabel("Time (s)")
 plt.ylabel("z-Score")
