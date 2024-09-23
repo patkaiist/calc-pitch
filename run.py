c3f67276f94ea6dfa5b8c84aec95db4dd34a1247
KP van Dam 柯禕藍 <patkaiist@gmail.com>
2024-09-23 09:06:38 +0200

    updated normalisation

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.interpolate import make_interp_spline
 
 import os
-folder = 'wai'
+folder = 'data'
 from os.path import isfile, join
 filelist = [f for f in os.listdir("./" + folder) if isfile(join("./" + folder, f))]
 for filename in filelist:
@@ -12,6 +12,9 @@ for filename in filelist:
 import pandas
 import math
 import matplotlib.pyplot as plt
+
+yscale = "ZScore"
+
 all_data = pandas.DataFrame()
 for filename in filelist:
     data = pandas.read_csv(folder + "/" + filename, sep="   ", engine="python")
@@ -19,26 +22,28 @@ for filename in filelist:
         lambda x: math.log(x / data["F0_Hz"].mean(), 2)
     )
     all_data = pandas.concat([all_data, data], ignore_index=True)
+
 plt.figure(figsize=(10, 6))
+
 for filename in filelist:
     data = pandas.read_csv("./" + folder + "/" + filename, sep="   ", engine="python")
     data["Semitones"] = 12 * data["F0_Hz"].apply(
-        lambda x: math.log(x / data["F0_Hz"].mean(), 2)
+        lambda x: math.log(x / all_data["F0_Hz"].mean(), 2)
     )
     data["Cents"] = round(data["Semitones"] * 100).astype(int)
     data["ZScore"] = (data["Semitones"] - all_data["Semitones"].mean()) / all_data["Semitones"].std()
     data["Time"] = data["Time_s"] - data["Time_s"].min()
 
     x = np.linspace(data["Time"].min(), data["Time"].max(), 300)
-    spl = make_interp_spline(data["Time"], data["ZScore"], k=2)
+    spl = make_interp_spline(data["Time"], data[yscale], k=2)
     y = spl(x)
 
-    plt.plot(x,y, label=filename)
+    plt.plot(x, y, label=filename)
 
 
 plt.title("log z-Score contours")
 plt.xlabel("Time (s)")
-plt.ylabel("z-Score")
-plt.legend(loc="upper right", fontsize='small')
+plt.ylabel("log z-Score")
+plt.legend(loc="lower right", fontsize='small')
 plt.grid(True)
 plt.savefig("./plot.png", dpi=300)
